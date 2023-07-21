b6ffee1e1260786b0792f12412469223d61dab13
CrisRockandRolla <ccdelamo@gmail.com>
2023-07-21 23:01:11 +0200

    añado getter y setter a clase Prueba

--- src/main/java/es/cic/Prueba.java
+++ src/main/java/es/cic/Prueba.java
@@ -3,15 +3,47 @@ package es.cic;
 public class Prueba {
 
     String id;
-    String nmombre;
+    String nombre;
     String apellidos;
 
     int edad;
 
-    public Prueba(String id, String nmombre, String apellidos, int edad) {
+    public Prueba(String id, String nombre, String apellidos, int edad) {
         this.id = id;
-        this.nmombre = nmombre;
+        this.nombre = nombre;
         this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 }
