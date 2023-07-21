1967b08a3637154f5bc62fd446732abce1c718a2
CrisRockandRolla <ccdelamo@gmail.com>
2023-07-22 01:09:03 +0200

    añado un test

--- src/test/java/es/cic/PruebaTest.java
+++ src/test/java/es/cic/PruebaTest.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PruebaTest {
+    Prueba prueba;
 
     @BeforeEach
     void setUp() {
+        prueba = new Prueba("1", "Cris", "Ajj", 22);
 
     }
 
     @Test
     void pt() {
-
+        prueba.getNombre();
     }
 
 }
