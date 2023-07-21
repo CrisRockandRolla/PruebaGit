9d8910056baa568e01745c7f8308c32eb586c4da
CrisRockandRolla <ccdelamo@gmail.com>
2023-07-21 22:38:56 +0200

    Relleno clase prueba

--- src/main/java/es/cic/Prueba.java
+++ src/main/java/es/cic/Prueba.java
@@ -1,4 +1,17 @@
 package es.cic;
 
 public class Prueba {
+
+    String id;
+    String nmombre;
+    String apellidos;
+
+    int edad;
+
+    public Prueba(String id, String nmombre, String apellidos, int edad) {
+        this.id = id;
+        this.nmombre = nmombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
 }
