91764ad872b095ec1efa7e1aedbd60e99acaf183
CrisRockandRolla <ccdelamo@gmail.com>
2023-07-21 23:51:03 +0200

    añado algunas dependencias necesarias para los test y para ejecutarlos en maven

---
+++ src/test/java/es/cic/PruebaTest.java
@@ -0,0 +1,18 @@
+package es.cic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PruebaTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void pt() {
+
+    }
+
+}
